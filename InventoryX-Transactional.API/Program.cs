49226a0ee7fec630c7da2e16c307f16c723adc69
Omar Gutierrez <omargtdev@gmail.com>
2023-10-22 12:18:13 -0500

    Injecting db

--- InventoryX-Transactional.API/Program.cs
+++ InventoryX-Transactional.API/Program.cs
@@ -1,6 +1,12 @@
+using InventoryX_Transactional.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<InventoryXDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("InventoryxDb"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
